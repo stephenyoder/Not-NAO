c3f774f9d397900586f807a85bb80f0e6b945e7d
stephenyoder <stephenayoder@gmail.com>
2017-11-02 18:12:38 -0700

    Add files via upload
    first commit

---
+++ Obstacle_Avoid_Basic/Obstacle_Avoid_Basic.ino
@@ -0,0 +1,83 @@
+#include <NewPing.h>
+#include <PololuQik.h>
+
+PololuQik2s12v10 qik(2, 3, 4);
+
+#define SONAR_NUM     10 // Number of sensors.
+#define MAX_DISTANCE 200 // Maximum distance (in cm) to ping.
+#define PING_INTERVAL 33 // Milliseconds between sensor pings (29ms is about the min to avoid cross-sensor echo).
+
+unsigned long pingTimer[SONAR_NUM]; // Holds the times when the next ping should happen for each sensor.
+unsigned int cm[SONAR_NUM];         // Where the ping distances are stored.
+uint8_t currentSensor = 0;          // Keeps track of which sensor is active.
+boolean obstacle = false;
+
+NewPing sonar[SONAR_NUM] = {     // Sensor object array.
+  NewPing(22, 23, MAX_DISTANCE), // Each sensor's trigger pin, echo pin, and max distance to ping.
+  NewPing(24, 25, MAX_DISTANCE),
+  NewPing(26, 27, MAX_DISTANCE),
+  NewPing(28, 29, MAX_DISTANCE),
+  NewPing(30, 31, MAX_DISTANCE),
+  NewPing(32, 33, MAX_DISTANCE),
+  NewPing(34, 35, MAX_DISTANCE),
+  NewPing(36, 37, MAX_DISTANCE),
+  NewPing(38, 39, MAX_DISTANCE),
+  NewPing(40, 41, MAX_DISTANCE)
+};
+
+void setup() {
+  qik.init();
+  
+  Serial.begin(115200);
+  
+  // Initialize timmers for sonar array
+  pingTimer[0] = millis() + 75;           // First ping starts at 75ms, gives time for the Arduino to chill before starting.
+  for (uint8_t i = 1; i < SONAR_NUM; i++) // Set the starting time for each sensor.
+    pingTimer[i] = pingTimer[i - 1] + PING_INTERVAL;
+    
+  qik.setM0Speed(64);
+  qik.setM1Speed(-64);
+}
+
+void loop() {
+  for (uint8_t i = 0; i < SONAR_NUM; i++) { // Loop through all the sensors.
+    if (millis() >= pingTimer[i]) {         // Is it this sensor's time to ping?
+      pingTimer[i] += PING_INTERVAL * SONAR_NUM;  // Set next time this sensor will be pinged.
+      if (i == 0 && currentSensor == SONAR_NUM - 1) oneSensorCycle(); // Sensor ping cycle complete, do something with the results.
+      sonar[currentSensor].timer_stop();          // Make sure previous timer is canceled before starting a new ping (insurance).
+      currentSensor = i;                          // Sensor being accessed.
+      cm[currentSensor] = MAX_DISTANCE;                      // Make distance zero in case there's no ping echo for this sensor.
+      sonar[currentSensor].ping_timer(echoCheck); // Do the ping (processing continues, interrupt will call echoCheck to look for echo).
+    }
+  }
+  // Other code that *DOESN'T* analyze ping results can go here.
+}
+
+void echoCheck() { // If ping received, set the sensor distance to array.
+  if (sonar[currentSensor].check_timer())
+    cm[currentSensor] = sonar[currentSensor].ping_result / US_ROUNDTRIP_CM;
+}
+
+void oneSensorCycle() { // Sensor ping cycle complete, do something with the results.
+  // The following code would be replaced with your code that does something with the ping results.
+  obstacle = false;
+  
+  for (uint8_t i = 3; i < 5; i++) {
+    if(cm[i] <= 150)
+      obstacle = true;
+    Serial.print(i);
+    Serial.print("=");
+    Serial.print(cm[i]);
+    Serial.print("cm ");
+  }
+  Serial.println();
+
+  if(obstacle) {
+    qik.setM0Speed(0);
+    qik.setM1Speed(0);
+  }
+  else {
+    qik.setM0Speed(64);
+    qik.setM1Speed(-64);
+  }
+}
